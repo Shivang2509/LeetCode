dbcfdb8b0804fc2565100754c67d76183c168105
Shivang Gupta <iamshivang25@gmail.com>
2026-07-12 20:12:55 +0530

    Time: 2 ms (19.1%), Space: 12.3 MB (89.13%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n>0:
+            if (n & (n-1))==0:
+                return True
+
+            else:
+                return False
+
+        else:
+                return False
+
+        
